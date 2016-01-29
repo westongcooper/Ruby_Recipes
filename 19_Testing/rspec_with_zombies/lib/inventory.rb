fd9250bdcff105aa6667bd4d62249393ae0ef6b2
Weston Cooper <weston@westoncooper.com>
2016-01-29 17:13:39 -0500

    extracted rspec_with_zombies into it's own respository

--- 19_Testing/rspec_with_zombies/lib/inventory.rb
+++
@@ -1,4 +0,0 @@
-class Item < ActiveRecord::Base
-  belongs_to :zombie
-  belongs_to :weapon
-end
--- 19_Testing/rspec_with_zombies/lib/weapon.rb
+++
@@ -1,4 +0,0 @@
-class Weapon < ActiveRecord::Base
-  validates :name, presence: true
-  has_many :zombies, through: :items
-end
